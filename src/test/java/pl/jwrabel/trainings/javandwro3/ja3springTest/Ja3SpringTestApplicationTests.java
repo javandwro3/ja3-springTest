776ae498ea168818af9bf7be8c93eb8cb4c8c8f4
Jakub Wrąbel <wrabel.jakub@gmail.com>
2017-05-31 19:02:53 +0200

    init

---
+++ src/test/java/pl/jwrabel/trainings/javandwro3/ja3springTest/Ja3SpringTestApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.jwrabel.trainings.javandwro3.ja3springTest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Ja3SpringTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
